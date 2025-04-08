1a43d24e9a285b1ac4b6150b9911c61d9464a998
niewyspany <triss@tuta.io>
2025-04-08 16:20:33 +0200

    Make eeg interface members private

--- eeg/eeg_device_interface.py
+++ eeg/eeg_device_interface.py
@@ -17,35 +17,35 @@ class EEGDeviceInterfaceError(Exception):
 class EEGDeviceInterface:
     def __init__(self, device_name: str, adapter_number: int):
         core.init()
-        self.manager = EEGManager()
-        self.device_name = device_name
-        self.adapter_number = adapter_number
-        self.device_features = DeviceFeatures()
+        self._manager = EEGManager()
+        self._device_name = device_name
+        self._adapter_number = adapter_number
+        self._device_features = DeviceFeatures()
 
     def __del__(self):
         self.disconnect()
 
     def connect(self):
         self._scan_for_devices()
-        status = self.manager.connect(self._get_device_index())
+        status = self._manager.connect(self._get_device_index())
         if status == 1:
             raise EEGDeviceInterfaceError("Connection failed")
         if status == 2:
             raise EEGDeviceInterfaceError("Stream is incompatible. Update the firmware.")
 
     def disconnect(self):
-        self.manager.disconnect()
+        self._manager.disconnect()
 
     """ Device status and hardware information """
 
     def get_sample_frequency(self) -> int:
-        if self.manager.is_connected():
-            return self.manager.get_sample_frequency()
+        if self._manager.is_connected():
+            return self._manager.get_sample_frequency()
         raise EEGDeviceInterfaceError("Device was not connected yet")
 
     def get_device_info(self) -> DeviceInfo:
-        if self.manager.is_connected():
-            info = self.manager.get_device_info()
+        if self._manager.is_connected():
+            info = self._manager.get_device_info()
             device_model = info.device_model
             hw_version = DeviceVersion(
                 major=info.hardware_version.major,
@@ -68,8 +68,8 @@ class EEGDeviceInterface:
         raise EEGDeviceInterfaceError("Device was not connected yet")
 
     def get_battery_status(self) -> BatteryStatus:
-        if self.manager.is_connected():
-            info = self.manager.get_battery_info()
+        if self._manager.is_connected():
+            info = self._manager.get_battery_info()
             return BatteryStatus(
                 level=info.level,
                 is_charging=info.is_charging,
@@ -81,19 +81,19 @@ class EEGDeviceInterface:
 
     def electrode_count_matches_cap(self, cap: Cap) -> bool:
         self._update_device_features()
-        return len(cap.mapping) == self.device_features.electrode_count
+        return len(cap.mapping) == self._device_features.electrode_count
 
     """ Device connection """
 
     def _scan_for_devices(self) -> None:
         try:
-            core.scan(self.adapter_number)
+            core.scan(self._adapter_number)
         except BrainAccessException as e:
             raise EEGDeviceInterfaceError(f"Device scan failed due to SDK error: {e}") from e
 
     def _get_device_index(self) -> int:
         for device_index in range(core.get_device_count()):
-            if self.device_name in core.get_device_name(device_index):
+            if self._device_name in core.get_device_name(device_index):
                 return device_index
 
         raise EEGDeviceInterfaceError("Device not found")
@@ -101,8 +101,8 @@ class EEGDeviceInterface:
     """ Utils """
 
     def _update_device_features(self) -> None:
-        features = self.manager.get_device_features()
-        self.device_features.has_gyro = features.has_gyro()
-        self.device_features.has_accel = features.has_accel()
-        self.device_features.is_bipolar = features.is_bipolar()
-        self.device_features.electrode_count = features.electrode_count()
+        features = self._manager.get_device_features()
+        self._device_features.has_gyro = features.has_gyro()
+        self._device_features.has_accel = features.has_accel()
+        self._device_features.is_bipolar = features.is_bipolar()
+        self._device_features.electrode_count = features.electrode_count()
