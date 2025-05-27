28249fe6fe047e16d146926f4ef8ed5ebff83a8c
niewyspany <triss@tuta.io>
2025-05-27 20:29:03 +0200

    Add signal recording option to the interface
    Feature #17

--- eeg/eeg_device_interface.py
+++ eeg/eeg_device_interface.py
@@ -2,7 +2,9 @@ import threading
 import time
 
 from brainaccess import core
+from brainaccess.core import eeg_channel
 from brainaccess.core.eeg_manager import EEGManager
+from brainaccess.core.gain_mode import GainMode
 from brainaccess.utils.exceptions import BrainAccessException
 
 from eeg.battery_status import BatteryStatus
@@ -10,6 +12,7 @@ from eeg.caps import Cap
 from eeg.device_features import DeviceFeatures
 from eeg.device_info import DeviceInfo
 from eeg.device_version import DeviceVersion
+from eeg.eeg_data_recorder import EEGDataRecorder
 
 
 class EEGDeviceInterfaceError(Exception):
@@ -24,9 +27,12 @@ class EEGDeviceInterface:
         self._device_name = device_name
         self._adapter_number = adapter_number
         self._device_features = DeviceFeatures()
+        self._eeg_data_recorder = EEGDataRecorder()
+        self._eeg_data_ready = False
         self._lock = threading.Lock()
 
     def close(self):
+        print("Device will be disconnect")
         self.disconnect()
         time.sleep(1)  # TODO: Nasty workaround to allow BLE callbacks to finish before shutdown
         core.close()
@@ -94,6 +100,68 @@ class EEGDeviceInterface:
         with self._lock:
             self._update_device_features()
             return len(cap.mapping) == self._device_features.electrode_count
+
+    """ EEG data streaming """
+
+    def start_stream(self) -> None:
+        self._eeg_data_ready = False
+        if not self._manager.is_streaming():
+            self._disable_all_channels()
+            self._enable_all_channels()
+            print("Manager not started, so it will be.")
+            self._eeg_data_recorder.set_sampling_rate(self.get_sample_frequency())
+            print("Sampling rate was set")
+            self._manager.set_callback_chunk(self._eeg_data_recorder.process_chunk)
+
+            # SDK has a problem with calling callback passed to the start/stop stream functions
+            if self._manager.start_stream():
+                print("Stream was successfully started")
+            else:
+                print("There was a problem with starting stream")
+
+    def stop_stream(self) -> None:
+        if self._manager.is_streaming():
+            # SDK has a problem with calling callback passed to the start/stop stream functions
+            if self._manager.stop_stream():
+                self._on_stream_stopped()
+            else:
+                self._disable_all_channels()
+        else:
+            print("Manager is not streaming yet")
+
+    def save_stream_to_file(self, path: str) -> None:
+        if self._eeg_data_ready:
+            print(f"Stream will be saved to the: {path}")
+            self._eeg_data_recorder.save_to_csv(path)
+        else:
+            print("EEG data not ready yet.")
+
+    def is_stream_data_ready(self):
+        return self._eeg_data_ready
+
+    def _enable_all_channels(self):
+        for ch in range(self._device_features.electrode_count):
+            ch_id = eeg_channel.ELECTRODE_MEASUREMENT + ch
+            self._manager.set_channel_enabled(ch_id, True)
+            self._manager.set_channel_gain(ch_id, GainMode.X12)
+
+        self._manager.set_channel_enabled(eeg_channel.SAMPLE_NUMBER, True)
+        self._manager.set_channel_enabled(eeg_channel.STREAMING, True)
+
+    def _disable_all_channels(self):
+        for ch in range(self._device_features.electrode_count):
+            ch_id = eeg_channel.ELECTRODE_MEASUREMENT + ch
+            self._manager.set_channel_enabled(ch_id, False)
+
+        self._manager.set_channel_enabled(eeg_channel.SAMPLE_NUMBER, False)
+        self._manager.set_channel_enabled(eeg_channel.STREAMING, False)
+
+    """ Callbacks """
+
+    def _on_stream_stopped(self):
+        print("Stream stopped")
+        self._disable_all_channels()
+        self._eeg_data_ready = True
 
     """ Device connection """
 
